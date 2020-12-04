a30106c2d08c28c19686ef5494925a4e22c6e32a
BeaT <helvis.tics00@gmail.com>
2020-12-04 19:32:30 +0200

    API and lecture buttons

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,26 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity, Image, Dimensions } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image, Dimensions, ActivityIndicator, FlatList } from 'react-native';
+
+//https://lekcijas.va.lv/lekcijas_android/getLecturesForSelectedDay.php?date=2020-12-4&program=IT1
+
+export default function App({navigation, route}) {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [date, setDate] = useState(getCurrentDate())
 
 
-export default function App({navigation}) {
+
+  //gets todays date and formats it for API
+  function getCurrentDate(){
+      var date = new Date().getDate();
+      var month = new Date().getMonth() + 1;
+      var year = new Date().getFullYear();
+
+      return year + '-' + month + '-' + date; //format: yyyy-mm-dd
+  }
+
+  //checks if there are lectures in selected day, outputs data accordingly
+  function isEmpty(item){
+    if (item.kurss == null){
+      return (
+          <TouchableOpacity style = {styles.mainButton}>
+            <Text style={styles.mainButtonText2}>There's no lectures today!</Text>
+          </TouchableOpacity>
+      );
+    } else {
+      return(
+        <TouchableOpacity style = {styles.mainButton}>
+          <Text style={styles.mainButtonText}>{item.kurss}</Text>
+          <Text style={styles.mainButtonText}>{item.sakums} - {item.beigas}</Text>
+          <Text style={styles.mainButtonText}>{item.nosaukums}, {item.lektors}</Text>
+        </TouchableOpacity>
+      );
+    }
+  }
+
+
+  // gets data from API
+  useEffect(() => {
+    fetch('https://lekcijas.va.lv/lekcijas_android/getLecturesForSelectedDay.php?date='+date+'&program='+route.params.kurss+route.params.gads)
+      .then((response) => response.json())
+      .then((json) => setData(json.result))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+
+
   return (
     <React.Fragment>
-
       {/* setting button */}
       <View style = {{backgroundColor:'white'}}>
         <TouchableOpacity onPress = {() => navigation.navigate('Settings')} style = {styles.settingButtonImg}>
           <Image
-          style = {styles.settingButtonImg}
-          source={require('../Resources/icons/settingBtn.png')}
+            style = {styles.settingButtonImg}
+            source={require('../Resources/icons/settingBtn.png')}
           />
         </TouchableOpacity>
       </View>
-      
-      {/* do what ever here */}
+
       <View style={styles.container}>
-        {/* CODE_STUFF */}
+
+        {/*pagaidu texts, var nemt nost ja grib*/}
+        <Text> Programmas izvēle: {route.params.kurss},  Kursa izvēle: {route.params.gads}</Text>
+        <Text>šodienas datums: {date}</Text>
+
+        {/*lekciju pogas*/}
+        {isLoading ? <ActivityIndicator/> : (
+          <FlatList
+            data={data}
+            keyExtractor={({ sakums }, index) => sakums}
+            renderItem={({ item }) => (
+              isEmpty(item)
+            )}
+          />
+        )}
+
       </View>
+      <StatusBar style="auto" />
     </React.Fragment>
   );
 }
@@ -49,5 +110,22 @@ const styles = StyleSheet.create({
     marginRight:'2%',
     marginTop:'2%',
     marginLeft:'90%'
+  },
+  //lekciju pogas styles
+  mainButton:{
+    backgroundColor:'#3399FF',
+    marginTop:'0.4%',
+    marginBottom:'0.4%',
+    padding:'1%'
+  },
+  //text for main button if there are lectures
+  mainButtonText:{
+    textAlign: 'center',
+  },
+  //text for main button if there are no lectures
+  mainButtonText2:{
+    textAlign: 'center',
+    height:screenHeight/10,
+    paddingTop:'6.5%'
   }
 });
--- Screens/setKurss.js
+++ Screens/setKurss.js
@@ -1,22 +1,111 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Picker, Button, TouchableOpacity, SafeAreaView, TextInput} from 'react-native';
+import {useState} from 'react';
 
+const setKurss = ({navigation}) => {
+  const [userKurss, setUserKurss] = useState();
+  const [userGads, setUserGads] = useState();
 
-export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Šī lapa parādās tikai ja iestatijumos nav uzstādīts tavs kurss</Text>
-      <Text>Vajazdētu rādīties pirmo reizi atverot app</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container1}>
+      <Text> Izvēlies savu studiju programmu: </Text>
+        <Picker
+          selectedValue={userKurss}
+          style={{height: 50, width: 150}}
+          onValueChange={(userkurss) => {
+            setUserKurss(userkurss);
+          }
+        }>
+        <Picker.Item label= "IZVĒLIES" value= "default" />
+        <Picker.Item label= "BV" value= "BV" />
+        <Picker.Item label= "nBV" value = "nBV"/>
+        <Picker.Item label= "BVV" value= "BVV" />
+        <Picker.Item label= "ESME" value= "ESME" />
+        <Picker.Item label= "IT" value= "IT" />
+        <Picker.Item label= "eIT" value= "eIT" />
+        <Picker.Item label= "ITk" value="ITk" />
+        <Picker.Item label= "KI" value= "KI"/>
+        <Picker.Item label= "KECE" value= "KECE"/>
+        <Picker.Item label= "KSA" value= "KSA" />
+        <Picker.Item label= "MSZ" value= "MSZ" />
+        <Picker.Item label= "MIP" value= "MIP"/>
+        <Picker.Item label= "MT" value= "MT" />
+        <Picker.Item label= "MTk" value= "MTk"  />
+        <Picker.Item label= "PI" value= "PI" />
+        <Picker.Item label= "SSM" value= "SSM" />
+        <Picker.Item label= "ITEM" value= "ITEM"/>
+        <Picker.Item label= "SKP" value= "SKP" />
+        <Picker.Item label= "TOV" value= "TOV"  />
+        <Picker.Item label= "TSV" value= "TSV"/>
+        <Picker.Item label= "TG" value= "TG" />
+        <Picker.Item label= "VRVT" value= "VRVT"/>
+
+        </Picker>
+        <Text> Izvēlies savu kursu: </Text>
+        <Picker
+          selectedValue={userGads}
+          style={{height: 50, width: 150}}
+          onValueChange={(usergads) => {
+            setUserGads(usergads);
+          }
+          }>
+          <Picker.Item label= "IZVĒLIES" value= "default" />
+          <Picker.Item label= "1.kurss" value= "1"/>
+          <Picker.Item label= "2.kurss" value= "2"/>
+          <Picker.Item label= "3.kurss" value= "3"/>
+          <Picker.Item label= "4.kurss" value= "4"/>
+          <Picker.Item label= "5.kurss" value= "5"/>
+        </Picker>
+        </View>
+        <View>
+        <Button
+        title = 'ESMU IZVĒLĒJIES'
+        color = 'black'
+          onPress={() =>
+            navigation.navigate('Home', {
+              kurss: userKurss,
+              gads: userGads,
+            })
+          }
+        />
+
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default setKurss;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  inputStyle: {
+    width: '80%',
+    height: 44,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#DBDBD6',
+  },
+  container1: {
+    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
   },
 });
