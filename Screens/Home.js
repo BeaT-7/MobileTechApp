1ead55e1a8f9209e118114b677d709adbd6aaef4
kristaapsm <kristaps.muurnieks@gmail.com>
2020-12-08 19:04:08 +0200

    Settings page

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,86 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity, Image, Dimensions, ActivityIndicator, FlatList } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 
 //https://lekcijas.va.lv/lekcijas_android/getLecturesForSelectedDay.php?date=2020-12-4&program=IT1
 
-export default function App({navigation, route}) {
+export default function App({ navigation, route }) {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  const [date, setDate] = useState(getCurrentDate())
-
-
+  const [date, setDate] = useState(getCurrentDate());
 
   //gets todays date and formats it for API
-  function getCurrentDate(){
-      var date = new Date().getDate();
-      var month = new Date().getMonth() + 1;
-      var year = new Date().getFullYear();
+  function getCurrentDate() {
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
 
-      return year + '-' + month + '-' + date; //format: yyyy-mm-dd
+    return year + "-" + month + "-" + date; //format: yyyy-mm-dd
   }
 
   //checks if there are lectures in selected day, outputs data accordingly
-  function isEmpty(item){
-    if (item.kurss == null){
+  function isEmpty(item) {
+    if (item.kurss == null) {
       return (
-          <TouchableOpacity style = {styles.mainButton}>
-            <Text style={styles.mainButtonText2}>There's no lectures today!</Text>
-          </TouchableOpacity>
+        <TouchableOpacity style={styles.mainButton}>
+          <Text style={styles.mainButtonText2}>There's no lectures today!</Text>
+        </TouchableOpacity>
       );
     } else {
-      return(
-        <TouchableOpacity style = {styles.mainButton}>
+      return (
+        <TouchableOpacity style={styles.mainButton}>
           <Text style={styles.mainButtonText}>{item.kurss}</Text>
-          <Text style={styles.mainButtonText}>{item.sakums} - {item.beigas}</Text>
-          <Text style={styles.mainButtonText}>{item.nosaukums}, {item.lektors}</Text>
+          <Text style={styles.mainButtonText}>
+            {item.sakums} - {item.beigas}
+          </Text>
+          <Text style={styles.mainButtonText}>
+            {item.nosaukums}, {item.lektors}
+          </Text>
         </TouchableOpacity>
       );
     }
   }
 
-
   // gets data from API
   useEffect(() => {
-    fetch('https://lekcijas.va.lv/lekcijas_android/getLecturesForSelectedDay.php?date='+date+'&program='+route.params.kurss+route.params.gads)
+    fetch(
+      "https://lekcijas.va.lv/lekcijas_android/getLecturesForSelectedDay.php?date=" +
+        date +
+        "&program=" +
+        route.params.kurss +
+        route.params.gads
+    )
       .then((response) => response.json())
       .then((json) => setData(json.result))
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   }, []);
 
-  
-  return(
+  return (
     <React.Fragment>
       {/* setting button */}
-      <View style = {{backgroundColor:'white'}}>
-      <Text> Programmas izvēle: {route.params.kurss} Kursa izvēle: {route.params.gads}</Text>
-        <TouchableOpacity onPress = {() => navigation.navigate('Settings')} style = {styles.settingButtonImg}>
+      <View style={{ backgroundColor: "white" }}>
+        <Text>
+          {" "}
+          Programmas izvēle: {route.params.kurss} Kursa izvēle:{" "}
+          {route.params.gads}
+        </Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Settings")}
+          style={styles.settingButtonImg}
+        >
           <Image
-            style = {styles.settingButtonImg}
-            source={require('../Resources/icons/settingBtn.png')}
+            style={styles.settingButtonImg}
+            source={require("../Resources/icons/settingBtn.png")}
           />
         </TouchableOpacity>
       </View>
 
-
       <View style={styles.container}>
-
         {/*pagaidu texts, var nemt nost ja grib*/}
-        <Text> Programmas izvēle: {route.params.kurss},  Kursa izvēle: {route.params.gads}</Text>
-        <Text>šodienas datums: {date}</Text>
+        <Text>
+          {" "}
+          Programmas izvēle: {route.params.kurss}, Kursa izvēle:{" "}
+          {route.params.gads}
+        </Text>
+        <Text>Šodienas datums: {date}</Text>
 
         {/*lekciju pogas*/}
-        {isLoading ? <ActivityIndicator/> : (
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
           <FlatList
             data={data}
             keyExtractor={({ sakums }, index) => sakums}
-            renderItem={({ item }) => (
-              isEmpty(item)
-            )}
+            renderItem={({ item }) => isEmpty(item)}
           />
         )}
-
       </View>
       <StatusBar style="auto" />
     </React.Fragment>
@@ -88,45 +112,44 @@ export default function App({navigation, route}) {
 }
 
 //gets screen parameters
-const screenWidth = Math.round(Dimensions.get('window').width);
-const screenHeight = Math.round(Dimensions.get('window').height); //not used at the moment
+const screenWidth = Math.round(Dimensions.get("window").width);
+const screenHeight = Math.round(Dimensions.get("window").height); //not used at the moment
 
 //gets ratio for setting button
-const settingBtnRatio = screenWidth/6
-
+const settingBtnRatio = screenWidth / 6;
 
 //Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'white',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    justifyContent: "center",
   },
   //setting button style
-  settingButtonImg:{
-    alignSelf: 'flex-end',
-    width:settingBtnRatio,
-    height:settingBtnRatio,
-    backgroundColor:'white',
-    marginRight:'2%',
-    marginTop:'2%',
-    marginLeft:'90%'
+  settingButtonImg: {
+    alignSelf: "flex-end",
+    width: settingBtnRatio,
+    height: settingBtnRatio,
+    backgroundColor: "white",
+    marginRight: "2%",
+    marginTop: "2%",
+    marginLeft: "90%",
   },
   //lekciju pogas styles
-  mainButton:{
-    backgroundColor:'#3399FF',
-    marginTop:'0.4%',
-    marginBottom:'0.4%',
-    padding:'1%'
+  mainButton: {
+    backgroundColor: "#3399FF",
+    marginTop: "0.4%",
+    marginBottom: "0.4%",
+    padding: "1%",
   },
   //text for main button if there are lectures
-  mainButtonText:{
-    textAlign: 'center',
+  mainButtonText: {
+    textAlign: "center",
   },
   //text for main button if there are no lectures
-  mainButtonText2:{
-    textAlign: 'center',
-    height:screenHeight/10,
-    paddingTop:'6.5%'
-  }
+  mainButtonText2: {
+    textAlign: "center",
+    height: screenHeight / 10,
+    paddingTop: "6.5%",
+  },
 });
--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,63 +1,147 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Switch } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Switch, Picker, Button } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
 
+const setKurss = ({ navigation }) => {
+  const [userKurss, setUserKurss] = useState();
+  const [userGads, setUserGads] = useState();
 
-
-
-
-export default function App() {
   //priekš paziņojumu pogas
   const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => 
-    !previousState);
-
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
   return (
-    <View style={pazinojumi.container} >
-      <Text>Paziņojumi</Text>
-      {/*<StatusBar style="auto" />*/}
-    {/* es nahuj nevaru sita jobana poga negrib iet pa labi lai ko es daru, to es pabeighsu velak )*/}
-        <Switch style={{aligncontent:'flex-end'}}
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-        
-      />
-   
+    <React.Fragment>
+      <View style={pazinojumi.container}>
+        <Text>Paziņojumi</Text>
+        {/*<StatusBar style="auto" />*/}
+        {/* es nahuj nevaru sita jobana poga negrib iet pa labi lai ko es daru, to es pabeighsu velak )*/}
+        <Switch
+          style={{ aligncontent: "flex-end" }}
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
 
-    
-    </View>
+      <View style={zindeks1.con3}>
+        <Text>Izvēlies savu studiju programmu:</Text>
+
+        <DropDownPicker
+          items={[
+            { label: "BV", value: "BV" },
+            { label: "nBV", value: "nBV" },
+            { label: "BVV", value: "BVV" },
+            { label: "ESME", value: "ESME" },
+            { label: "IT", value: "IT" },
+            { label: "eIT", value: "eIT" },
+            { label: "ITk", value: "ITk" },
+            { label: "KI", value: "KI" },
+            { label: "KECE", value: "KECE" },
+            { label: "KSA", value: "KSA" },
+            { label: "MSZ", value: "MSZ" },
+            { label: "MIP", value: "MIP" },
+            { label: "MT", value: "MT" },
+            { label: "MTk", value: "MTk" },
+            { label: "PI", value: "PI" },
+            { label: "SSM", value: "SSM" },
+            { label: "ITEM", value: "ITEM" },
+            { label: "SKP", value: "SKP" },
+            { label: "TOV", value: "TOV" },
+            { label: "TSV", value: "TSV" },
+            { label: "TG", value: "TG" },
+            { label: "VRVT", value: "VRVT" },
+          ]}
+          selectedValue={userGads}
+          style={{ height: 50, width: 150 }}
+          onValueChange={(usergads) => {
+            setUserGads(usergads);
+          }}
+          //kka jadabu no pirmas lapas defaultValue={userKurss}
+          placeholder="Izvēlies savu kursu"
+          containerStyle={{ height: 40 }}
+          style={{ backgroundColor: "#ffffff" }}
+          dropDownStyle={{ backgroundColor: "white" }}
+          selectedValue={userKurss}
+          onChangeItem={(userKurss) => setUserKurss(userKurss.value)}
+        />
+      </View>
+
+      <View style={zindeks.con3}>
+        <Text> Izvēlies savu kursu: </Text>
+        <DropDownPicker
+          items={[
+            { label: "1.kurss", value: "1" },
+            { label: "2.kurss", value: "2" },
+            { label: "3.kurss", value: "3" },
+            { label: "4.kurss", value: "4" },
+            { label: "5.kurss", value: "5" },
+          ]}
+          //kka jadabu no pirmas lapas defaultValue={userKurss}
+          placeholder="Izvēlies savu gadu:"
+          containerStyle={{ height: 40 }}
+          style={{ backgroundColor: "#ffffff" }}
+          dropDownStyle={{ backgroundColor: "white" }}
+          selectedValue={userGads}
+          onChangeItem={(userGads) => setUserGads(userGads.value)}
+        />
+      </View>
+
+      <View style={pazinojumi.container}>
+        <Button
+          title="Saglabāt izmaiņas"
+          color="black"
+          onPress={() =>
+            navigation.navigate("Home", {
+              kurss: userKurss,
+              gads: userGads,
+            })
+          }
+        />
+      </View>
+    </React.Fragment>
   );
-}
-
-
+};
+export default setKurss;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-   flexDirection:'row',
+    flexDirection: "row",
     //alignItems: 'center',
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
 });
 
 const pazinojumi = StyleSheet.create({
   container: {
-    flexDirection:'row',
-    alignItems: 'flex-end',
+    flexDirection: "row",
+    alignItems: "flex-end",
     padding: 16,
-    marginTop:16,
-    borderColor: '#bbb',
+    marginTop: 16,
+    borderColor: "#bbb",
     borderWidth: 1,
-    borderStyle: 'dashed',
-    borderRadius:10
-  }
+    borderStyle: "dashed",
+    borderRadius: 10,
+  },
 });
 
-const padding = StyleSheet.create({
-  con:{
-    paddingTop: 1
-  }
-})
+const zindeks = StyleSheet.create({
+  con3: {
+    zIndex: 3, // works on ios
+    elevation: 3, // works on android
+  },
+});
 
+const zindeks1 = StyleSheet.create({
+  con3: {
+    zIndex: 5, // works on ios
+    elevation: 5, // works on android
+  },
+});
+const padding = StyleSheet.create({
+  con: {
+    paddingTop: 1,
+  },
+});
